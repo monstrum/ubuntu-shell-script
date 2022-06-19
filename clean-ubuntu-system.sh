822cb2fd57b71de89847a14fc4d9a82fdf12cc33
Erwin Eu <eu.erwin@xie-group.com>
2022-06-19 09:24:18 +0200

    Add clean snap package caches

--- clean-ubuntu-system.sh
+++ clean-ubuntu-system.sh
@@ -9,3 +9,14 @@ composer clear-cache
 sudo rm -rf /var/log/*.gz
 sudo rm -rf /var/log/*.old
 sudo rm -rf /var/log/**/*.gz
+
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
+sudo rm -rf /var/lib/snapd/cache/*
